d2b62c4c3e3037efcb578f36eaf63d9c40502728
Naresh Kumar Rai <n.rai@student.fontys.nl>
2022-12-15 13:32:22 +0100

    Added new app: tasks -common layout -csrf verification

--- lecture3/lecture3/urls.py
+++ lecture3/lecture3/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include("hello.urls")),
     path('newyear/', include("newyear.urls")),
-    path('practice/', include("practice.urls"))
+    path('practice/', include("practice.urls")),
+    path('tasks/', include("tasks.urls"))
 ]
