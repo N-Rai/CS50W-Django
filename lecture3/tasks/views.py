08290adc67384ec76329452b4ced7f5fe48dcb19
Naresh Kumar Rai <n.rai@student.fontys.nl>
2022-12-16 16:14:13 +0100

    Added django forms -Client side validation -Redirect

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -1,6 +1,14 @@
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
 
 tasks = ["foo", "bar", "baz"]
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    # priority = forms.IntegerField(label="Priority", max_value=10, min_value=1)
+
 # Create your views here.
 def index(request):
     return render(request, "tasks/index.html",{
@@ -8,5 +16,15 @@ def index(request):
     })
 
 def add(request):
-    return render(request, "tasks/add.html")
+    if request.method == "POST":            #check if the request method is POST
+        form = NewTaskForm(request.POST)    #Set all the input data to a variable
+        if form.is_valid():                 #chcek if all the form data is valid
+            task = form.cleaned_data["task"]    # If so retrieve the task 
+            tasks.append(task)	                # Add the task to the list of tasks
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks.html", {"form": form})
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
 
